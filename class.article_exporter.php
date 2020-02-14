358994df9018facb4231f2148ef746f304b1d248
karelwintersky <karel.wintersky@yandex.ru>
2020-02-14 05:54:06 +0300

    Экспорт: медиа, тайтл-фото, html-виджеты

---
+++ class.article_exporter.php
@@ -0,0 +1,197 @@
+<?php
+
+use Arris\DB;
+
+class ArticleExporter
+{
+    /**
+     * @var array Датасет для экспорта
+     */
+    private $_dataset = [];
+
+    /**
+     * @var array Сет медиаресурсов
+     */
+    private $_media = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function export()
+    {
+        $this->_dataset = [
+            'oldid'     =>  $this->id,                                     // id статьи
+            'cdate'     =>  date('c', strtotime($this->cdate)),     // ISO Date
+            'type'      =>  $this->type,                                    // тип
+            'creator'   =>  [                                               // информация об авторе
+                'id'        =>  $this->author_id,                           // ID
+                'login'     =>  $this->author_login,                        // логин
+                'sign'      =>  self::_trim($this->author, 'TRIM.AUTHOR')                               // подпись
+            ],
+            'status'    =>  [                                               // статус статьи в базе
+                'is_hidden' =>  $this->s_hidden,                            // установлен флаг "скрытая"
+                'is_draft'  =>  $this->s_draft,                             // установлен флаг "черновик"
+            ],
+            'title'     =>  self::_trim($this->title, 'TRIM.TITLE'),
+            'lead'      =>  self::_trim($this->short, 'TRIM.LEAD'),
+            'media'     =>  [
+                'title'     =>  $this->parseMediaTitle(),
+                'media'     =>  $this->parseMediaPhotos(),
+                // 'reports'   =>  [],
+                'html'      =>  $this->parseHTMLWidgets()
+            ],
+            'text_bb'   =>  $this->text_bb,                                 // исходный текст, размеченный BB-кодами
+        ];
+
+        return $this->_dataset;
+    }
+
+    /**
+     * Возвращает MEDIA типа 'titleimage'
+     *
+     * @return array
+     */
+    private function parseMediaTitle()
+    {
+        $u_photo = ($this->photo != '') ? @unserialize($this->photo) : [];
+
+        $_media_title = [
+            'type'  =>  'title'
+        ];
+
+        $is_present = false;
+
+        if (!empty($u_photo) && is_array($u_photo)) {
+            if (array_key_exists('file', $u_photo) && array_key_exists('path', $u_photo)) {
+                $_media_title['paths'] = [
+                    stripslashes($u_photo['path']) . '/' . $u_photo['file']
+                ];
+                $is_present = true;
+            }
+
+            if ($is_present && array_key_exists('descr', $u_photo)) {
+                $_media_title['titles'] = [ $u_photo['descr'] ];
+            }
+        }
+
+        if (getenv('MEDIA.TITLE.EXPORT_RAW')) {
+            $_media_title['raw'] = $u_photo;
+        }
+
+        if (getenv('MEDIA.TITLE.EXPORT_ALWAYS') == 0 && !$is_present) {
+            $_media_title = [];
+        }
+
+        return $_media_title;
+    }
+
+    private function parseMediaPhotos()
+    {
+        $_data = [];
+
+        /*
+        select af.*, files.*
+from articles_files as af
+left join files on files.id = af.fid
+where af.item = 119
+         */
+
+        $query = "
+SELECT 
+       af.id AS embed_id,
+       af.fid AS mediafile_id,
+       af.descr AS media_title,
+       files.name AS media_alttext,
+       files.cdate,
+       files.type,
+       files.file,
+       files.wm,
+       files.source AS media_source,
+       files.link AS media_link,
+       files.status,
+       files.status_size
+FROM 
+     articles_files AS af 
+LEFT JOIN files ON files.id = af.fid 
+WHERE af.item = {$this->id}
+        ";
+
+        $sth = DB::query($query);
+        $_data['_'] = 0;
+
+        while ($photo = $sth->fetch()) {
+            $photo['path'] = date('Y/m/', strtotime($photo['cdate'])) . $photo['file'];
+
+            $_data[$photo['embed_id']] = $photo;
+            $_data['_']++;
+        }
+
+        return $_data;
+    }
+
+    /**
+     * Применяет trim если установлена соотв. переменная конфига
+     *
+     * @param $value
+     * @param $env
+     * @return string
+     */
+    private static function _trim($value, $env)
+    {
+        return getenv($env) ? trim($value) : $value;
+    }
+
+    /**
+     * Добавляет информацию по HTML-виджетам
+     */
+    private function parseHTMLWidgets()
+    {
+        $_data = [];
+
+        // 'YTowOnt9' - это закодированное 'a:0:{}'
+        // 'N;' - null
+        // 'Tjs=' - эквивалентно 'N;'
+        // чаще всего данные будут base64
+        /*if ($this->html === ''
+            || $this->html === 'YTowOnt9'
+            || $this->html === 'a:0:{}'
+            || $this->html === 'Tjs='
+            || $this->html === 'N;'
+            || $this->html === ''
+        ) return false; // виджетов нет
+        */
+
+        $html = self::_isBase64($this->html) ? base64_decode($this->html) : $this->html;
+        $html = unserialize($html);
+
+        if (!$html) return [];
+
+        $_data['_'] = count($html);
+
+        foreach ($html as $id => $code) {
+            $set = [];
+
+            if (getenv('MEDIA.HTML.EXPORT_STRING'))
+                $set['html'] = $code;
+
+            if (getenv('MEDIA.HTML.EXPORT_BASE64'))
+                $set['base64'] = base64_encode($code);
+
+            $_data[$id] = $set;
+        }
+
+        if (getenv('MEDIA.HTML.SAVE_DEBUG')) {
+            $this->_dataset['debug:html'] = $html;
+        }
+
+        return $_data;
+    }
+
+    private static function _isBase64($string)
+    {
+        return (base64_decode($string, true) !== false);
+    }
+
+}
---
+++ export.php
@@ -0,0 +1,70 @@
+#!/usr/bin/php
+<?php
+ini_set('memory_limit','256M');
+
+use Arris\AppLogger;
+use Arris\DB;
+use Monolog\Logger;
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/class.article_exporter.php';
+
+Dotenv::create(__DIR__, '_env')->load();
+
+AppLogger::init('FFI_EXPORT', basename(__FILE__));
+AppLogger::addScope('main', [
+    [ 'export.log', Logger::DEBUG ]
+]);
+
+try {
+    DB::init(NULL, [
+        'database'  =>  getenv('DB_DATABASE'),
+        'username'  =>  getenv('DB_USERNAME'),
+        'password'  =>  getenv('DB_PASSWORD'),
+        'charset'   =>  'UTF8'
+    ], AppLogger::scope('mysql'));
+
+    $select_query = "SELECT id FROM articles ORDER BY id";
+    // $select_query = "SELECT id FROM articles WHERE id IN(19757, 20004, 20353, 20748, 21143) ORDER BY id";
+
+    $articles_ids_list = DB::query($select_query)->fetchAll(PDO::FETCH_COLUMN);
+    $count_curr  = 0;
+    $count_total = count($articles_ids_list);
+
+    foreach ($articles_ids_list as $id) {
+        // $filename = "export/{$id}.json";
+        $filename = "export/" . str_pad($id, 5, '0', STR_PAD_LEFT) . '.json';
+        $count_curr++;
+
+        $query_get_article = "
+SELECT
+	a.*,
+    adm.login AS author_login
+FROM 
+	articles AS a
+LEFT JOIN admin AS adm ON a.author_id = adm.id 	
+WHERE
+	a.id = {$id}
+";
+
+        /**
+         * @class ArticleExporter $article
+         */
+        $article = DB::query($query_get_article)->fetchObject('ArticleExporter');
+
+        $json = $article->export();
+
+        file_put_contents($filename, json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES), LOCK_EX);
+
+        $message
+            = "[" . str_pad($count_curr, 6, ' ', STR_PAD_LEFT)
+            . " / " . str_pad($count_total, 6, ' ', STR_PAD_RIGHT) . ']' .
+            " Article id = <font color='green'>{$id}</font> exported to file <font color='yellow'>{$filename}</font>";
+
+        \Arris\CLIConsole::say($message);
+    }
+
+} catch (Exception $e) {
+    dd($e->getMessage());
+}
