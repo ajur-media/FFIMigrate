f07d5de4fde13dbd910a7edfa85010b22b1d4918
karelwintersky <karel.wintersky@yandex.ru>
2020-03-10 17:37:51 +0300

    check file_exist() for title image - final

--- classes/class.ExportArticle.php
+++ classes/class.ExportArticle.php
@@ -172,13 +172,18 @@ WHERE
 
             if (array_key_exists('file', $u_photo) && array_key_exists('path', $u_photo) && $u_photo['file'] && $u_photo['path']) {
 
-                FFIECommon::_check_file($_media_title['uri'], stripslashes($u_photo['path']) . '/' . $u_photo['file']);
+                $basepath = getenv('PATH.STORAGE') . stripslashes($u_photo['path']) . '/';
+
+                FFIECommon::_check_file($_media_title['uri'],  $basepath. $u_photo['file']);
+
                 $is_present = true;
 
             } elseif (array_key_exists('file', $u_photo) && array_key_exists('cdate', $u_photo) && $u_photo['file'] && $u_photo['cdate']) {
 
-                $basepath = getenv('PATH.STORAGE') . 'photos/' . $u_photo['cdate'] . '/';
+                $basepath = getenv('PATH.STORAGE') . 'photos/' . date('Y/m/', strtotime($u_photo['cdate']));
+
                 FFIECommon::_check_file($_media_title['predicted'], $basepath . $u_photo['file']);
+
                 $is_present = true;
             }
 
